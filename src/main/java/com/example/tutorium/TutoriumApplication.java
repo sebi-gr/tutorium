ff9874d35967614e0fc52023daf2b26279e28ffe
Sebastian Gruenewald <wi21b080@technikum-wien.at>
2023-06-25 10:18:37 +0200

    Initial commit

---
+++ src/main/java/com/example/tutorium/TutoriumApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoriumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoriumApplication.class, args);
+	}
+
+}
